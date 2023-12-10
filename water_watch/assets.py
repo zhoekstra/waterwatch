51841e0dd4e7801d0186e8e59f4f903ebcbad7a4
Zachary Hoekstra <zhoekstra@gmail.com>
2023-12-10 22:52:46 +0100

    add 7d asset flows

--- water_watch/assets.py
+++ water_watch/assets.py
@@ -4,14 +4,21 @@ import dateutil.parser
 import pandas
 import requests
 from dagster import asset, MultiPartitionsDefinition, StaticPartitionsDefinition, HourlyPartitionsDefinition, \
-    AssetExecutionContext, build_asset_context, MultiPartitionKey
+    AssetExecutionContext, build_asset_context, MultiPartitionKey, WeeklyPartitionsDefinition, AssetIn, \
+    LastPartitionMapping, MultiToSingleDimensionPartitionMapping
 
-from water_watch.stateflow_schema import SiteFlowInformation, SiteFlowFile, NULL_DATETIME_STRING
+from water_watch.stateflow_schema import SiteFlowInformation, SiteFlowFile, NULL_DATETIME_STRING, \
+    SiteFlowAverageInformation, SiteFlowAverageFile
 from water_watch.io_managers import gcs_io_manager_key, bq_io_manager_key
 
 HourlyStatePartititonDefenition = MultiPartitionsDefinition({
     "state": StaticPartitionsDefinition(["co"]),
     "date": HourlyPartitionsDefinition(start_date="2023-12-10-07:00"),
+})
+
+WeeklyStatePartititonDefenition = MultiPartitionsDefinition({
+    "state": StaticPartitionsDefinition(["co"]),
+    "date": WeeklyPartitionsDefinition(start_date="2023-12-10-07:00"),
 })
 
 
@@ -56,3 +63,62 @@ def site_flow_information(context: AssetExecutionContext,
     result['_state'] = partition['state']
     result['_runtime'] = dateutil.parser.isoparse(partition['date']).astimezone(timezone.utc)
     return result
+
+
+@asset(
+    io_manager_key=gcs_io_manager_key,
+    partitions_def=WeeklyStatePartititonDefenition)
+def flow_data_7d_raw(context: AssetExecutionContext) -> str:
+    partition: dict[str, str] = context.partition_key.keys_by_dimension
+    r = requests.get(f"https://waterwatch.usgs.gov/webservices/flows7d?region={partition['state']}&format=json")
+    return r.text
+
+
+@asset(
+    partitions_def=WeeklyStatePartititonDefenition)
+def flow_data_7d_parsed(flow_data_7d_raw: str) -> list[SiteFlowAverageInformation]:
+    return SiteFlowAverageFile.from_json(flow_data_7d_raw).sites
+
+
+@asset(
+    io_manager_key=bq_io_manager_key,
+    partitions_def=WeeklyStatePartititonDefenition,
+    metadata={"partition_expr": {'date': '_runtime', 'state': '_state'}})
+def site_flow_7d_information(context: AssetExecutionContext,
+                             flow_data_7d_parsed: list[SiteFlowInformation]) -> pandas.DataFrame:
+    partition: dict[str, str] = context.partition_key.keys_by_dimension
+    result = pandas.DataFrame(current_flow_data_parsed)
+    result.rename(columns={'class_': 'class'})
+    # pull flow/stage status codes out into a separate column
+    result['flow_status'] = result['flow'].apply(lambda f: f if isinstance(f, str) else 'Nrm')
+    result['flow'] = result['flow'].apply(lambda f: None if isinstance(f, str) else f)
+    # filter out null datetimes and parse the rest
+    result['flow_dt'] = result['flow_dt'].apply(
+        lambda t: None if t == NULL_DATETIME_STRING else dateutil.parser.isoparse(t).astimezone(timezone.utc))
+    # parse non-null percent_*
+    result['percent_median'] = result['percent_median'].apply(lambda f: float(f) if f else None)
+    result['percent_mean'] = result['percent_mean'].apply(lambda f: float(f) if f else None)
+    # add partition columns
+    result['_state'] = partition['state']
+    result['_runtime'] = dateutil.parser.isoparse(partition['date']).astimezone(timezone.utc)
+    return result
+
+
+@asset(
+    io_manager_key=bq_io_manager_key,
+    metadata={"partition_expr": {'date': '_runtime', 'state': '_state'}},
+    ins={"site_flow_7d_information":
+             AssetIn(["site_flow_7d_information"],
+                     partition_mapping=MultiToSingleDimensionPartitionMapping(partition_dimension_name="state")),
+         "site_flow_information":
+             AssetIn(["site_flow_information"],
+                     partition_mapping=MultiToSingleDimensionPartitionMapping(partition_dimension_name="state"))
+         })
+def sites(site_flow_7d_information: pandas.DataFrame, site_flow_information: pandas.DataFrame) -> pandas.DataFrame:
+    columns_to_retain = ['site_no', 'station_nm',
+                         'dec_lat_va', 'dec_long_va',
+                         'huc_cd', 'tz_cd',
+                         'class_']
+    reduced_7d = site_flow_7d_information[columns_to_retain]
+    reduced_flow = site_flow_information[columns_to_retain]
+    return pandas.concat([reduced_7d, reduced_flow], ignore_index=True).drop_duplicates()
--- water_watch/stateflow_schema.py
+++ water_watch/stateflow_schema.py
@@ -7,6 +7,7 @@ from marshmallow import fields
 
 NULL_DATETIME_STRING = "0000-00-00 00:00:00"
 
+
 @dataclass(frozen=True)
 class SiteFlowInformation(DataClassJsonMixin):
     site_no: str
@@ -27,6 +28,29 @@ class SiteFlowInformation(DataClassJsonMixin):
     percent_mean: Optional[str]
     url: str
 
+
 @dataclass(frozen=True)
 class SiteFlowFile(DataClassJsonMixin):
     sites: list[SiteFlowInformation]
+
+
+@dataclass(frozen=True)
+class SiteFlowAverageInformation(DataClassJsonMixin):
+    site_no: str
+    station_nm: str
+    dec_lat_va: float
+    dec_long_va: float
+    huc_cd: str
+    tz_cd: str
+    flow: Union[str, float]
+    flow_dt: Optional[str]
+    flow_unit: str
+    flow_nday: int
+    class_: int = field(metadata=config(field_name="class"))
+    percent_median: Optional[str]
+    percent_mean: Optional[str]
+
+
+@dataclass(frozen=True)
+class SiteFlowAverageFile(DataClassJsonMixin):
+    sites: list[SiteFlowAverageInformation]
