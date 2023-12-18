296b2540ca7e89e414050441ce3bff7b050e8685
Zachary Hoekstra <zhoekstra@gmail.com>
2023-12-18 07:27:56 +0100

    everything in gc

--- water_watch/__init__.py
+++ water_watch/__init__.py
@@ -10,10 +10,10 @@ from . import assets
 from .io_managers import gcs_io_manager_key, bq_io_manager_key, gcs_io_manager, bq_io_manager
 
 
-# AUTH_FILE = "/tmp/gcp_creds.json"
-# with open(AUTH_FILE, "w") as f:
-#     json.dump(json.loads(base64.b64decode(os.getenv("GCP_CREDS_JSON_CREDS_BASE64"))), f)
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = AUTH_FILE
+AUTH_FILE = "/tmp/gcp_creds.json"
+with open(AUTH_FILE, "w") as f:
+    json.dump(json.loads(base64.b64decode(os.getenv("GCP_CREDS_JSON_CREDS_BASE64"))), f)
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = AUTH_FILE
 
 all_assets = load_assets_from_modules(
     modules=[assets],
