b64900644f9d9535a0482cf45cd965c3689d6701
Zachary Hoekstra <zhoekstra@gmail.com>
2023-12-18 07:37:29 +0100

    update iomanagers

--- water_watch/__init__.py
+++ water_watch/__init__.py
@@ -2,17 +2,18 @@ import base64
 import json
 import os
 
-from dagster import Definitions, load_assets_from_modules, FilesystemIOManager, AutoMaterializePolicy, \
+from dagster import Definitions, load_assets_from_modules, AutoMaterializePolicy, \
     AutoMaterializeRule
 from dagster_gcp import GCSResource
 
 from . import assets
-from .io_managers import gcs_io_manager_key, bq_io_manager_key, gcs_io_manager, bq_io_manager
+from .io_managers import gcs_io_manager_key, bq_io_manager_key, gcs_io_manager, bq_io_manager, filesystem_io_manager
 
-
-AUTH_FILE = "/tmp/gcp_creds.json"
+AUTH_FILE = "./gcp_creds.json"
 with open(AUTH_FILE, "w") as f:
-    json.dump(json.loads(base64.b64decode(os.getenv("GCP_CREDS_JSON_CREDS_BASE64"))), f)
+    base64creds = os.getenv("GCP_CREDS_JSON_CREDS_BASE64")
+    creds_str = json.loads(base64.b64decode(base64creds)) if base64creds else ''
+    json.dump(creds_str, f)
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = AUTH_FILE
 
 all_assets = load_assets_from_modules(
@@ -26,7 +27,7 @@ defs = Definitions(
     assets=all_assets,
     resources={
         "gcs": GCSResource(project="waterwatch-406910"),
-        "io_manager": FilesystemIOManager(base_dir="local_data"),
+        "io_manager": filesystem_io_manager,
         gcs_io_manager_key: gcs_io_manager,
         bq_io_manager_key: bq_io_manager,
     },
--- water_watch/io_managers.py
+++ water_watch/io_managers.py
@@ -1,11 +1,17 @@
-from dagster import EnvVar
+import os
+
+from dagster import EnvVar, FilesystemIOManager
 from dagster_gcp import GCSPickleIOManager, GCSResource
 from dagster_gcp_pandas import BigQueryPandasIOManager
+ENV = os.getenv('DAGSTER_CLOUD_DEPLOYMENT_NAME', default='unknown')
+
+filesystem_io_manager_key = "io_manager"
+filesystem_io_manager = FilesystemIOManager(base_dir=f"data_{ENV}")
 
 bq_io_manager_key = "BQ_IO_MANAGER"
 bq_io_manager = BigQueryPandasIOManager(
     project="waterwatch-406910",
-    dataset="waterwatch_dev",
+    dataset=f"waterwatch_{ENV}",
     timeout=15.0
 )
 
@@ -13,5 +19,5 @@ gcs_io_manager_key = "GCS_IO_MANAGER"
 gcs_io_manager = GCSPickleIOManager(
     gcs=GCSResource(project="waterwatch-406910"),
     gcs_bucket="dagster_assets",
-    gcs_prefix="water_watch"
+    gcs_prefix=f"water_watch/{ENV}"
 )
